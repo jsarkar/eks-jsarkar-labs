cbbb9b594e41fd1cc68dce34905750183e21e440
jsarkar <joy623227@gmail.com>
2021-05-09 11:21:47 -0400

    debug

---
+++ tf/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "12.2.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.17"
+  subnets         = [var.subnets]
+
+  vpc_id = var.vpc_id
+
+  node_groups = {
+    first = {
+      desired_capacity = 3
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = "t2.micro"
+    }
+  }
+
+  write_kubeconfig   = true
+  config_output_path = "./"
+}
---
+++ tf/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnets" {
+  type = string
+}
+variable "cluster_name" {
+}
